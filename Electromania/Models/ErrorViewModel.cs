078ac86b3d3407b08a1e50fab7343e4f17725a97
tomga1 <tomasgarcia003@gmail.com>
2025-03-19 16:39:39 -0300

    Agregar archivos de proyecto.

---
+++ Electromania/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Electromania.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
